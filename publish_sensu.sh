7a192ad437d2705cc7039a89b0104f0e64026748
Sam Kottler <shk@linux.com>
2014-06-30 10:49:27 +0000

    (publish_sensu.sh) add name to the create_snapshot call

--- publish_sensu.sh
+++ publish_sensu.sh
@@ -4,6 +4,6 @@
 
 SNAPSHOT=$(get_dated_snapshot_name sensu)
 
-create_snapshot_from_name $SNAPSHOT
+create_snapshot_from_name sensu $SNAPSHOT
 drop_published_mirror sensu
 publish_snapshot_from_name $SNAPSHOT
